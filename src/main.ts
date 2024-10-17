ee4bcec75174a759abe8f25803c0af29e5372ccb
Arun George <aruniverse@users.noreply.github.com>
2024-10-17 14:17:11 -0400

    ch 1

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import {
   BentleyCloudRpcManager,
   IModelReadRpcInterface,
   IModelTileRpcInterface,
-  QueryBinder,
-  QueryRowFormat,
 } from "@itwin/core-common";
 import {
   CheckpointConnection,
@@ -18,14 +16,10 @@ import {
   StandardViewTool,
   ViewCreator3d,
 } from "@itwin/core-frontend";
-import { PresentationRpcInterface } from "@itwin/presentation-common";
-import { Presentation } from "@itwin/presentation-frontend";
-import { RealityDataAccessClient } from "@itwin/reality-data-client";
 
 const {
   IMJS_ACCESS_TOKEN,
   IMJS_BING_MAPS_KEY,
-  IMJS_CESIUM_ION_KEY,
   IMJS_ITWIN_ID,
   IMJS_IMODEL_ID,
 } = import.meta.env;
@@ -34,14 +28,9 @@ const authClient: AuthorizationClient = {
   getAccessToken: async () => IMJS_ACCESS_TOKEN,
 };
 
-const rdClient = new RealityDataAccessClient({
-  authorizationClient: authClient,
-});
-
 const rpcInterfaces = [
   IModelReadRpcInterface,
   IModelTileRpcInterface,
-  PresentationRpcInterface,
 ];
 
 BentleyCloudRpcManager.initializeClient(
@@ -62,42 +51,12 @@ await IModelApp.startup({
       value: IMJS_BING_MAPS_KEY,
     },
   },
-  tileAdmin: {
-    cesiumIonKey: IMJS_CESIUM_ION_KEY,
-  },
-  realityDataAccess: rdClient,
 });
-
-await Presentation.initialize();
 
 const iModelConnection = await CheckpointConnection.openRemote(
   IMJS_ITWIN_ID,
   IMJS_IMODEL_ID
 );
-
-iModelConnection.selectionSet.onChanged.addListener(async (ev) => {
-  // console.log("Selection set changed", ev);
-  if (!ev.set.elements.size) {
-    return;
-  }
-  for (const elementId of ev.set.elements.values()) {
-    for await (const row of iModelConnection.createQueryReader(
-      "SELECT * FROM bis.Element WHERE ECInstanceId = ?",
-      QueryBinder.from([elementId]),
-      {
-        rowFormat: QueryRowFormat.UseECSqlPropertyNames,
-      }
-    )) {
-      console.log(`ECInstanceId is ${row[0]}`);
-      console.log(`ECClassId is ${row.ecclassid}`);
-    }
-    const properties = await Presentation.presentation.getElementProperties({
-      imodel: iModelConnection,
-      elementId,
-    });
-    console.log(properties);
-  }
-});
 
 const root = document.querySelector<HTMLDivElement>("#app")!;
 
@@ -109,21 +68,3 @@ IModelApp.viewManager.addViewport(vp);
 
 IModelApp.tools.run(StandardViewTool.toolId, vp, StandardViewId.RightIso);
 IModelApp.tools.run(FitViewTool.toolId, vp, true, false);
-
-const REALITY_DATA_URL =
-  "https://api.bentley.com/reality-management/reality-data";
-
-const { realityDatas } = await rdClient.getRealityDatas(
-  IMJS_ACCESS_TOKEN,
-  IMJS_ITWIN_ID,
-  undefined
-);
-console.log({ realityDatas });
-
-for (const rd of realityDatas) {
-  // const tilesetUrl = await rdClient.getRealityDataUrl(IMJS_ITWIN_ID, rd.id);
-  const tilesetUrl = `${REALITY_DATA_URL}/${rd.id}?iTwinId=${IMJS_ITWIN_ID}`;
-  vp.displayStyle.attachRealityModel({
-    tilesetUrl,
-  });
-}
